2816d604d9d92ca47bf17a9e97a1ae586745f35e
katrina <katrina.nov13@gmail.com>
2021-11-26 16:41:23 +0300

    refact input

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -4,86 +4,74 @@ import {Button} from "./components/Button";
 import {Input} from "./components/Input";
 
 type TaskType = {
-    id: string
-    title: string
-    isDone: boolean
+  id: string
+  title: string
+  isDone: boolean
 }
 
 type PropsType = {
-    title: string
-    tasks: Array<TaskType>
-    removeTask: (taskId: string) => void
-    changeFilter: (value: FilterValuesType) => void
-    addTask: (title: string) => void
+  title: string
+  tasks: Array<TaskType>
+  removeTask: (taskId: string) => void
+  changeFilter: (value: FilterValuesType) => void
+  addTask: (title: string) => void
 }
 
 export function Todolist(props: PropsType) {
 
-    const [title, setTitle] = useState('')
+  const [title, setTitle] = useState('')
 
-    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.currentTarget.value)
-    }
-
-    const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-        // if ((event.key === 'Control')|| event.key==='Enter'){
-         if (event.ctrlKey) {
-            addTaskHandler()
-        }
-    }
-    const addTaskHandler = () => {
-        props.addTask(title)
-        setTitle('')
-    }
-
-    const removeTaskHandler = (id: string) => {
-        props.removeTask(id)
-    }
-
-    // const onClickAllHandler = () => props.changeFilter('all')
-    // const onClickActiveHandler = () => props.changeFilter('all')
-    // const onClickCompletedHandler = () => props.changeFilter('all')
-
+  const addTaskHandler = () => {
+    props.addTask(title)
+    setTitle('')
+  }
+  const removeTaskHandler = (id: string) => {
+    props.removeTask(id)
+  }
   const tsarFooHandler = (value: FilterValuesType) => {
     props.changeFilter(value)
   }
-return <div>
+  return <div>
     <h3>{props.title}</h3>
     <div>
-        <Input value={title} callbackChange={onChangeHandler} callbackKeyPress={onKeyPressHandler}/>
-        {/*<input value={title} onChange={onChangeHandler} onKeyPress={onKeyPressHandler}/>*/}
-        {/*<button onClick={addTaskHandler}>+</button>*/}
-        <Button name={'+'} callback={addTaskHandler}/>
+      <Input
+        value={title}
+        callbackChange={setTitle}
+        callbackKeyPress={addTaskHandler}/>
+      <Button name={'+'} callback={addTaskHandler}/>
+
+      {/*<input value={title} onChange={onChangeHandler} onKeyPress={onKeyPressHandler}/>*/}
+      {/*<button onClick={addTaskHandler}>+</button>*/}
 
     </div>
     <ul>
-        {
-            props.tasks.map(t => <li>
-                <input type="checkbox" checked={t.isDone}/>
-                <span>{t.title}</span>
-                {/*<button onClick={() => removeTaskHandler(t.id)}>x</button>*/}
-              <Button name={'x'} callback={() => removeTaskHandler(t.id)}/>
-                {/*<button onClick={() => {props.removeTask(t.id)}}>x</button>*/}
-            </li>)
-        }
+      {
+        props.tasks.map(t => <li>
+          <input type="checkbox" checked={t.isDone}/>
+          <span>{t.title}</span>
+          {/*<button onClick={() => removeTaskHandler(t.id)}>x</button>*/}
+          <Button name={'x'} callback={() => removeTaskHandler(t.id)}/>
+          {/*<button onClick={() => {props.removeTask(t.id)}}>x</button>*/}
+        </li>)
+      }
     </ul>
     <div>
-        {/*<button onClick={() => tsarFooHandler('all')}>All</button>*/}
-        {/*<button onClick={() => tsarFooHandler('active')}>Active</button>*/}
-        {/*<button onClick={() => tsarFooHandler('completed')}>Completed</button>*/}
+      {/*<button onClick={() => tsarFooHandler('all')}>All</button>*/}
+      {/*<button onClick={() => tsarFooHandler('active')}>Active</button>*/}
+      {/*<button onClick={() => tsarFooHandler('completed')}>Completed</button>*/}
 
-        <Button name={'all'} callback={() => tsarFooHandler('all')}/>
-        <Button name={'active'} callback={() => tsarFooHandler('active')}/>
-        <Button name={'completed'} callback={() => tsarFooHandler('completed')}/>
+      <Button name={'all'} callback={() => tsarFooHandler('all')}/>
+      <Button name={'active'} callback={() => tsarFooHandler('active')}/>
+      <Button name={'completed'} callback={() => tsarFooHandler('completed')}/>
 
 
-        {/*<button onClick={changeFilterAllHandler}>All</button>*/}
-        {/*/!*<button onClick={() => {props.changeFilter("all")}}>All</button>*!/*/}
-        {/*<button onClick={changeFilterActiveHandler}>Active</button>*/}
-        {/*/!*<button onClick={() => {props.changeFilter("active")}}>Active</button>*!/*/}
-        {/*<button onClick={changeFilterCompletedHandler}>Completed</button>*/}
-        {/*/!*<button onClick={() => {props.changeFilter("completed")}}>Completed</button>*!/*/}
+      {/*<button onClick={changeFilterAllHandler}>All</button>*/}
+      {/*/!*<button onClick={() => {props.changeFilter("all")}}>All</button>*!/*/}
+      {/*<button onClick={changeFilterActiveHandler}>Active</button>*/}
+      {/*/!*<button onClick={() => {props.changeFilter("active")}}>Active</button>*!/*/}
+      {/*<button onClick={changeFilterCompletedHandler}>Completed</button>*/}
+      {/*/!*<button onClick={() => {props.changeFilter("completed")}}>Completed</button>*!/*/}
     </div>
-</div>
+  </div>
 }
 
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,17 +2,21 @@ import React, {ChangeEvent, KeyboardEvent} from 'react';
 
 type propsType = {
   value: string
-  callbackChange: (event: ChangeEvent<HTMLInputElement>) => void
-  callbackKeyPress: (event: KeyboardEvent<HTMLInputElement>) => void
+  callbackChange: (event: string) => void
+  callbackKeyPress: () => void
 }
 export const Input = (props: propsType) => {
+
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    props.callbackChange(event);
+    props.callbackChange(event.currentTarget.value)
   }
   const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-    props.callbackKeyPress(event);
+    event.key === 'Enter' && props.callbackKeyPress()
   }
   return  (
-    <input value={props.value} onChange={onChangeHandler} onKeyPress={onKeyPressHandler}/>
+    <input
+      value={props.value}
+      onChange={onChangeHandler}
+      onKeyPress={onKeyPressHandler}/>
   )
 }
